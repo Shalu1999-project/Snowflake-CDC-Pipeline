dd27ce862195cf288269d87d9d74a8a1d0a2d026
Vaishali Ganotra <vaishalign76@gmail.com>
2026-07-26 22:18:46 +0530

    Delete 02_Create_Stream.sql

--- 02_Create_Stream.sql
+++
@@ -1,8 +0,0 @@
-/*=========================================================
-  Project : Snowflake CDC Pipeline
-  File    : 02_Create_Stream.sql
-  Purpose : Create stream on EMPLOYEE table
-=========================================================*/
-
-Create or replace stream Employee_Stream
-on table employee
