67d0a3a0815999f3b12b8bf0fef0083061d554e3
Vaishali Ganotra <vaishalign76@gmail.com>
2026-07-26 16:42:05 +0530

    Commit

---
+++ 04_Create_Audit_Table.sql
@@ -0,0 +1,10 @@
+ Create table employee_audit_new
+ (
+     AUDIT_TIME timestamp
+     ,EMP_ID int
+     ,EMP_NAME string
+     ,DEPARTMENT String
+     ,SALARY Number
+     ,ACTION string
+     ,IS_UPDATE boolean
+ )
